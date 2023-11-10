a386d9396dbd71aa6650a070584f018aaa879c18
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-10 21:10:19 +0530

    login

--- BloodDonation/pages/Login.js
+++ BloodDonation/pages/Login.js
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { View, Image, Text, TouchableOpacity, StyleSheet, TextInput, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import {firebase} from '../firebase/config'
-import MyImage from '../assets/logo2.jpg';
+import { firebase } from '../firebase/config'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MyImage from '../assets/banner.jpg';
 
 const Login = () => {
     const navigation = useNavigation()
-    const [email,setEmail] = useState('');
-    const [password, setPassword] =useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    loginUser = async (email,password) =>{
-        try{
-          await firebase.auth().signInWithEmailAndPassword(email,password)
-        }catch (error){
-          alert(error.message)
+    loginUser = async (email, password) => {
+        try {
+            await firebase.auth().signInWithEmailAndPassword(email, password)
+        } catch (error) {
+            alert(error.message)
         }
-      }
+    }
 
     const [showPassword, setShowPassword] = useState(false);
 
@@ -38,31 +39,36 @@ const Login = () => {
 
     return (
         <ScrollView contentContainerStyle={styles.container1}>
-            <View style={styles.container}>
-                <Image source={MyImage} style={styles.image} />
-            </View>
-            <Text style={styles.header}>Welcome, Sign In</Text>
+           <Image source={MyImage} style={styles.image} />
+            <Text style={styles.header}>Welcome</Text>
+            <Text style={styles.header1}>Sign In</Text>
             <Text style={styles.inputDetails}>Email</Text>
             <TextInput
                 placeholder="Enter Email"
                 style={styles.textBoxes}
                 onChangeText={(email) => setEmail(email)}
-        autoCapitalize='none'
-        autoCorrect={false}
+                autoCapitalize='none'
+                autoCorrect={false}
             />
             <Text style={styles.inputDetails}>Password</Text>
             <TextInput
                 placeholder="Enter Password" secureTextEntry={!showPassword}
                 style={styles.textBoxes}
-        value={password}
-        onChangeText={(password) => setPassword(password)}
-        autoCorrect={false}
+                value={password}
+                onChangeText={(password) => setPassword(password)}
+                autoCorrect={false}
             />
-            <TouchableOpacity onPress={togglePasswordVisibility}>
-                <Text style={styles.show}>{showPassword ? 'Hide Password' : 'Show Password'}</Text>
+            <TouchableOpacity onPress={togglePasswordVisibility} style={styles.eyeIcon}>
+                    <Icon name={showPassword ? 'eye' : 'eye-slash'} size={20} color="#777" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                <Text style={styles.show}>Forgot Password ?</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => loginUser(email,password)} style={styles.buttonStyle}>
-                <Text style={styles.buttonText}>Log In</Text>
+            <TouchableOpacity onPress={() => loginUser(email, password)} style={styles.buttonStyle}>
+                <Text style={styles.buttonText}>Sign In</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text style={styles.show2}>Don't have account ? Register</Text>
             </TouchableOpacity>
         </ScrollView>
     );
@@ -77,11 +83,13 @@ const styles = StyleSheet.create({
     container1: {
         flexGrow: 1, // Allows the content to expand within the ScrollView
         backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'left',
     },
     image: {
-        width: '65%',
-        height: '65%',
-        marginTop: '-27%',
+        width: 400,
+        height: 200,
+        alignItems:'center'
     },
     buttonText: {
         color: 'white',
@@ -122,18 +130,47 @@ const styles = StyleSheet.create({
         color: 'white',
     },
     header: {
-        fontSize: 30,
+        fontSize: 35,
         fontWeight: 'bold',
-        marginTop: '-20%',
+        marginTop: '-28%',
+        marginBottom: '30%',
+        color: '#E90505',
+        textAlign:'center'
+    },
+    header1: {
+        fontSize: 28,
+        fontWeight: 'bold',
         marginLeft: '5%',
-        marginBottom: '8%',
+        marginBottom: '2%',
         color: '#FF1515',
     },
     show: {
-        textAlign: 'right',
-        marginRight: 25,
-        marginTop: 10,
-        marginBottom: 10,
+        textAlign: 'left',
+        margin: 25,
+    },
+    show2: {
+        textAlign: 'center',
+        margin: 5,
+    },
+    passwordContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '90%',
+        marginLeft: 20,
+    },
+    passwordInput: {
+        flex: 1,
+        fontSize: 16,
+        padding: 12,
+        borderColor: '#F76363',
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    },
+    eyeIcon: {
+        paddingRight: "8%",
+        marginTop:'-13%',
+        alignItems:'flex-end'
     },
 });
 
