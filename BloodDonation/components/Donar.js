ea185bc07fa80bbd8b08b38d64c0f4357177ec34
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-20 10:50:03 +0530

    Update Donar.js

--- BloodDonation/components/Donar.js
+++ BloodDonation/components/Donar.js
@@ -1,147 +1,247 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, ScrollView, TextInput, TouchableOpacity } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { firebase } from '../firebase/config';
-import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { firebase } from "../firebase/config";
 
 const Donar = () => {
-  return (
-    <ScrollView contentContainerStyle={styles.scrollViewContent}>
-      <View style={styles.container}>
-        <Text style={styles.header}>Become a Blood Donar</Text>
-        <Text style={styles.note}>A blood donar can be found in the nearest area to the person who needs a blood donar by the distric and area.</Text>
-        <Text style={styles.inputDetails}>Your Blood Group</Text>
-        <View style={styles.pickerContainer}>
-          <Picker
-            // selectedValue={Btype}
-            // onValueChange={(itemValue) => setBtype(itemValue)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Select Your Blood Group" value='' />
-            <Picker.Item label="AA" value='AA' />
-            <Picker.Item label="A+" value="A+" />
-            <Picker.Item label="A-" value="A-" />
-            <Picker.Item label="B+" value="B+" />
-            <Picker.Item label="B-" value="B-" />
-            <Picker.Item label="AB+" value="AB+" />
-            <Picker.Item label="AB-" value="AB-" />
-            <Picker.Item label="O+" value="O+" />
-            <Picker.Item label="O-" value="O-" />
-          </Picker>
-        </View>
-        <Text style={styles.inputDetails}>Enter Your Contact No</Text>
-        <TextInput
-          placeholder="Enter Contact No"
-          style={styles.textBoxes}
-        //   onChangeText={(number) => setNumber(number)}
-        //   autoCapitalize="none"
-        //   autoCorrect={false}
-        />
-        <Text style={styles.inputDetails}>Your Distric</Text>
-        <View style={styles.pickerContainer}>
-          <Picker
-            // selectedValue={Btype}
-            // onValueChange={(itemValue) => setBtype(itemValue)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Select Your Distric" value='' />
-            <Picker.Item label="AA" value='AA' />
-            <Picker.Item label="A+" value="A+" />
-            <Picker.Item label="A-" value="A-" />
-            <Picker.Item label="B+" value="B+" />
-            <Picker.Item label="B-" value="B-" />
-            <Picker.Item label="AB+" value="AB+" />
-            <Picker.Item label="AB-" value="AB-" />
-            <Picker.Item label="O+" value="O+" />
-            <Picker.Item label="O-" value="O-" />
-          </Picker>
-        </View>
+  const [name, setName] = useState({});
+  const [dname, setdname] = useState("");
+  const [Btype, setBtype] = useState("");
+  const [number, setnumber] = useState("");
+  const [distric, setdistric] = useState("");
+  const [area, setarea] = useState("");
+  const [donarDetails, setdonarDetails] = useState([]);
 
-        <Text style={styles.inputDetails}>Enter Your Area</Text>
-        <TextInput
-          placeholder="Enter Contact No"
-          style={styles.textBoxes}
-        //   onChangeText={(number) => setNumber(number)}
-        //   autoCapitalize="none"
-        //   autoCorrect={false}
-        />
-        
-        <TouchableOpacity
-          style={styles.buttonStyle}
-        //   onPress={() => registerUser(email, password, name, Btype, number)}
-        >
-          <Text style={styles.buttonText}>Submit Details</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
-  )
-}
+  useEffect(() => {
+    const userUid = firebase.auth().currentUser.uid;
+    const userDocRef = firebase.firestore().collection("users").doc(userUid);
+
+    userDocRef.get().then((snapshot) => {
+      if (snapshot.exists) {
+        setName(snapshot.data());
+        setdname(name.name);
+        loadDonarDetails();
+      } else {
+        console.log("User doesn't exist");
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (donarDetails.length === 1) {
+      const [detail] = donarDetails;
+
+      setBtype(detail.Btype);
+      setnumber(detail.number);
+      setdistric(detail.distric);
+      setarea(detail.area);
+    }
+  }, [donarDetails]);
+
+  const loadDonarDetails = () => {
+    const userUid = firebase.auth().currentUser.uid;
+    const userDocRef = firebase.firestore().collection("users").doc(userUid);
+
+    userDocRef.get().then((snapshot) => {
+      if (snapshot.exists) {
+        const userData = snapshot.data();
+        if (userData.donar) {
+          setdonarDetails(userData.donar);
+        }
+      } else {
+        console.log("User doesn't exist");
+      }
+    });
+  };
+
+  const handleAddMarks = () => {
+    const userUid = firebase.auth().currentUser.uid;
+    const userDocRef = firebase.firestore().collection("users").doc(userUid);
+
+    const dname = name.name;
+
+    userDocRef
+      .update({
+        donar: firebase.firestore.FieldValue.arrayUnion({
+          dname: dname,
+          Btype: Btype,
+          number: number,
+          distric: distric,
+          area: area,
+        }),
+      })
+      .then(() => {
+        console.log("You become Blood Donar Successfully!!");
+        setdname("");
+        setBtype("");
+        setnumber("");
+        setdistric("");
+        setarea("");
+      })
+      .catch((error) => {
+        console.error("Error become Blood Donar:", error);
+      });
+  };
+  if (donarDetails.length === 1) {
+    const [donorDetail] = donarDetails;
+    return (
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          <Text>
+            {donorDetail.dname}: {donorDetail.distric}
+          </Text>
+        </View>
+      </ScrollView>
+    );
+  } else {
+    return (
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          <Text style={styles.header}>Become a Blood Donar</Text>
+          <Text style={styles.note}>
+            A blood donar can be found in the nearest area to the person who
+            needs a blood donar by the distric and area.
+          </Text>
+          <Text style={styles.inputDetails}>Your Blood Group</Text>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={Btype}
+              onValueChange={(itemValue1) => setBtype(itemValue1)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Select Your Blood Group" value="" />
+              <Picker.Item label="AA" value="AA" />
+              <Picker.Item label="A+" value="A+" />
+              <Picker.Item label="A-" value="A-" />
+              <Picker.Item label="B+" value="B+" />
+              <Picker.Item label="B-" value="B-" />
+              <Picker.Item label="AB+" value="AB+" />
+              <Picker.Item label="AB-" value="AB-" />
+              <Picker.Item label="O+" value="O+" />
+              <Picker.Item label="O-" value="O-" />
+            </Picker>
+          </View>
+          <Text style={styles.inputDetails}>Enter Your Contact No</Text>
+          <TextInput
+            placeholder="Enter Contact No"
+            style={styles.textBoxes}
+            value={number}
+            onChangeText={(text) => setnumber(text)}
+          />
+          <Text style={styles.inputDetails}>Your Distric</Text>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={distric}
+              onValueChange={(itemValue) => setdistric(itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Select Your Distric" value="" />
+              <Picker.Item label="Kegalle" value="Kegalle" />
+              <Picker.Item label="Rathnapura" value="Rathnapura" />
+              <Picker.Item label="A-" value="A-" />
+              <Picker.Item label="B+" value="B+" />
+              <Picker.Item label="B-" value="B-" />
+              <Picker.Item label="AB+" value="AB+" />
+              <Picker.Item label="AB-" value="AB-" />
+              <Picker.Item label="O+" value="O+" />
+              <Picker.Item label="O-" value="O-" />
+            </Picker>
+          </View>
+
+          <Text style={styles.inputDetails}>Enter Your Area</Text>
+          <TextInput
+            placeholder="Enter Contact No"
+            style={styles.textBoxes}
+            value={area}
+            onChangeText={(text) => setarea(text)}
+          />
+
+          <TouchableOpacity style={styles.buttonStyle} onPress={handleAddMarks}>
+            <Text style={styles.buttonText}>Submit Details</Text>
+          </TouchableOpacity>
+
+          {donarDetails.map((item, index) => (
+            <Text key={index}>
+              {item.dname}: {item.distric}
+            </Text>
+          ))}
+        </View>
+      </ScrollView>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      width: '100%',
-      backgroundColor: 'white',
-      padding:10
-    },
-    inputDetails: {
-      fontSize: 17,
-      marginBottom: '0%',
-    },
-    textBoxes: {
-      width: '94%',
-      fontSize: 16,
-      padding: 12,
-      borderColor: '#F76363',
-      borderWidth: 1,
-      borderRadius: 10,
-      backgroundColor: 'rgba(255, 255, 255, 0.8)',
-      margin:10
-    },
-    buttonStyle: {
-      backgroundColor: '#FF1515',
-      padding: 13,
-      borderRadius: 10,
-      width: '94%',
-      height: 50,
-      margin: 10,
-      marginBottom: 20,
-      borderColor: '#FF1515',
-      borderWidth: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: 'white',
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 17,
-      fontWeight: 'bold',
-    },
-    header: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginBottom: '1%',
-      color: '#FF1515',
-    },
-    note: {
-        fontSize: 15,
-        marginBottom: 10,
-        marginTop:10
-      },
-    scrollViewContent: {
-      flexGrow: 1,
-    },
-    pickerContainer: {
-      borderWidth: 1,
-      borderColor: '#F76363',
-      borderRadius: 10,
-      margin: 10,
-      backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    },
-    picker: {
-      width: '100%',
-    },
-  });
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "white",
+    padding: 10,
+  },
+  inputDetails: {
+    fontSize: 17,
+    marginBottom: "0%",
+  },
+  textBoxes: {
+    width: "94%",
+    fontSize: 16,
+    padding: 12,
+    borderColor: "#F76363",
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: "#FF1515",
+    padding: 13,
+    borderRadius: 10,
+    width: "94%",
+    height: 50,
+    margin: 10,
+    marginBottom: 20,
+    borderColor: "#FF1515",
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: "1%",
+    color: "#FF1515",
+  },
+  note: {
+    fontSize: 15,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  pickerContainer: {
+    borderWidth: 1,
+    borderColor: "#F76363",
+    borderRadius: 10,
+    margin: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+  },
+  picker: {
+    width: "100%",
+  },
+});
 
-export default Donar
+export default Donar;
