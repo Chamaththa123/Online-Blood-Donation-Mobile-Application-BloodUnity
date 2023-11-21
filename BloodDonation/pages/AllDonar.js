2fb197d73cc60f62a2f11c66028f934cbeab0323
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-22 00:14:08 +0530

    Update AllDonar.js

--- BloodDonation/pages/AllDonar.js
+++ BloodDonation/pages/AllDonar.js
@@ -6,9 +6,9 @@ import { firebase } from "../firebase/config";
 const AllDonors = () => {
   const [allDonors, setAllDonors] = useState([]);
   const [selectedBloodType, setSelectedBloodType] = useState("");
+  const [selectedDistrict, setSelectedDistrict] = useState("");
 
   useEffect(() => {
-    // Fetch all donor details from Firestore
     const fetchAllDonors = async () => {
       const donorsCollection = firebase.firestore().collection("users");
       const snapshot = await donorsCollection.where("donar", "!=", null).get();
@@ -29,15 +29,38 @@ const AllDonors = () => {
     fetchAllDonors();
   }, []);
 
-  const filteredDonors = selectedBloodType
-    ? allDonors.filter((donor) => donor.Btype === selectedBloodType)
-    : allDonors;
+  const filteredDonors = allDonors.filter((donor) => {
+    return (
+      (!selectedBloodType || donor.Btype === selectedBloodType) &&
+      (!selectedDistrict || donor.distric === selectedDistrict)
+    );
+  });
+
+  const sortedDonors = filteredDonors.sort((a, b) => {
+    if (a.distric !== b.distric) {
+      return a.distric.localeCompare(b.distric);
+    } else {
+      return a.Btype.localeCompare(b.Btype);
+    }
+  });
 
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContent}>
       <View style={styles.container}>
         <Text style={styles.header}>All Donors Details</Text>
-
+        <View style={styles.dropdownContainer}>
+          <Text>Select District: </Text>
+          <Picker
+            selectedValue={selectedDistrict}
+            style={{ height: 50, width: 150 }}
+            onValueChange={(itemValue) => setSelectedDistrict(itemValue)}
+          >
+            <Picker.Item label="Select District" value="" />
+            <Picker.Item label="Kegalle" value="Kegalle" />
+            <Picker.Item label="Rathnapura" value="Rathnapura" />
+            {/* Add other district options as needed */}
+          </Picker>
+        </View>
         <View style={styles.dropdownContainer}>
           <Text>Select Blood Type: </Text>
           <Picker
@@ -55,7 +78,7 @@ const AllDonors = () => {
         </View>
 
         {/* Display filtered donors based on the selected blood type */}
-        {filteredDonors.map((donor, index) => (
+        {sortedDonors.map((donor, index) => (
           <View key={index} style={styles.donorContainer}>
             <Text>Name: {donor.dname}</Text>
             <Text>Blood Type: {donor.Btype}</Text>
