5a17457a3397cbd442b4ecc7ccdf5dc35f69a8d2
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-22 23:00:16 +0530

    Update App.js

--- BloodDonation/App.js
+++ BloodDonation/App.js
@@ -1,60 +1,57 @@
-import React,{useEffect,useState} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {firebase} from './firebase/config';
-import Start from './pages/Start';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import HomePage from './pages/HomePage';
-import AllDonar from './pages/AllDonar';
-import DonarDetails from './pages/DonarDetails';
-
-
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { firebase } from "./firebase/config";
+import Start from "./pages/Start";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import HomePage from "./pages/HomePage";
+import AllDonar from "./pages/AllDonar";
+import DonarDetails from "./pages/DonarDetails";
 
 const Stack = createNativeStackNavigator();
 const App = () => {
-  const [initializing,setInitializing] = useState(true);
-  const [user,setUser] = useState();
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-  function onAuthStateChanged(user){
+  function onAuthStateChanged(user) {
     setUser(user);
-    if(initializing) setInitializing(false);
+    if (initializing) setInitializing(false);
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
-  },[]);
+  }, []);
 
   if (initializing) return null;
 
-  if(!user){
+  if (!user) {
     return (
-        <Stack.Navigator>
-          <Stack.Screen name='Start' component={Start} />
-          <Stack.Screen name='Login' component={Login} />
-          <Stack.Screen name='Register' component={Register} />
-          <Stack.Screen name='Home' component={HomePage} />
-          <Stack.Screen name='Profile' component={Profile} />
-        </Stack.Navigator>
-    ) 
+      <Stack.Navigator>
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    );
   }
   return (
     <Stack.Navigator>
-          <Stack.Screen name='Home' component={HomePage} />
-      <Stack.Screen name='Profile' component={Profile} />
-      <Stack.Screen name='AllDonar' component={AllDonar} />
-      <Stack.Screen name='DonorDetails' component={DonarDetails} />
+      <Stack.Screen name="Home" component={HomePage} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="AllDonar" component={AllDonar} />
+      <Stack.Screen name="DonorDetails" component={DonarDetails} />
     </Stack.Navigator>
-) 
+  );
 };
 
-
-export default () =>{
-  return(
+export default () => {
+  return (
     <NavigationContainer>
-      <App/>
+      <App />
     </NavigationContainer>
-  )
-}
+  );
+};
