6bce086bc61524425fb7d10ee2827523bdaa5bb2
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-27 11:41:56 +0530

    Update config.js

--- BloodDonation/firebase/config.js
+++ BloodDonation/firebase/config.js
@@ -1,18 +1,18 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore'
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0YHbfC_t21isTnwA-6aZ7e2FYrcMT_Sg",
-    authDomain: "blood-donation-ac142.firebaseapp.com",
-    projectId: "blood-donation-ac142",
-    storageBucket: "blood-donation-ac142.appspot.com",
-    messagingSenderId: "385846849363",
-    appId: "1:385846849363:web:3e1863ff8404f389f9fba6"
-  };
+  apiKey: "AIzaSyA0YHbfC_t21isTnwA-6aZ7e2FYrcMT_Sg",
+  authDomain: "blood-donation-ac142.firebaseapp.com",
+  projectId: "blood-donation-ac142",
+  storageBucket: "blood-donation-ac142.appspot.com",
+  messagingSenderId: "385846849363",
+  appId: "1:385846849363:web:3e1863ff8404f389f9fba6",
+};
 
-if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-export {firebase};
+export { firebase };
