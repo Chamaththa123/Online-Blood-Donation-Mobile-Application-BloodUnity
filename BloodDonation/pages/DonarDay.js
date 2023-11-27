58e0be438b4a1fa261d1eb575c94ef9b6537f2e2
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-27 11:39:13 +0530

    Update DonarDay.js

--- BloodDonation/pages/DonarDay.js
+++ BloodDonation/pages/DonarDay.js
@@ -22,21 +22,65 @@ const DonorDay = () => {
     <ScrollView contentContainerStyle={styles.scrollViewContainer}>
       <View style={styles.container}>
         <Image source={image5} style={styles.image} />
-        <Text style={styles.text}>World Blood Donor Day serves as a global recognition of the invaluable contribution made by blood donors worldwide. Observed on June 14th every year, it aims to raise awareness about the importance of voluntary blood donation and to acknowledge the selfless individuals who donate blood to save lives.</Text>
-        <Text style={styles.text}>BThe event was established by the World Health Organization (WHO) to highlight the constant need for safe blood and blood products for transfusion and to express gratitude to blood donors for their life-saving gifts. The theme of World Blood Donor Day varies each year, focusing on different aspects related to blood donation, transfusion, 
-        and health.</Text>
-        <Text style={styles.text}>The significance of blood donation cannot be overstated. Donated blood is vital for various medical procedures, including surgeries, childbirth, trauma care, and treating chronic illnesses such as anemia and cancer. It is also crucial for patients suffering from conditions like thalassemia and hemophilia who require regular transfusions 
-        to lead healthy lives.</Text>
-        <Text style={styles.text}>The theme for World Blood Donor Day often emphasizes raising awareness and encouraging more people, especially young individuals, to become regular blood donors. Educating communities about the importance of safe blood donation practices, addressing misconceptions, and promoting voluntary, unpaid donations are essential 
-        components of these campaigns.</Text>
-        <Text style={styles.text}>Donating blood is a simple yet impactful act that has the potential to save lives. It’s often said that a single donation can save up to three lives, as blood can be separated into its components – red blood cells, plasma, and platelets – and used for different patients in need.
+        <Text style={styles.text}>
+          World Blood Donor Day serves as a global recognition of the invaluable
+          contribution made by blood donors worldwide. Observed on June 14th
+          every year, it aims to raise awareness about the importance of
+          voluntary blood donation and to acknowledge the selfless individuals
+          who donate blood to save lives.
         </Text>
-        <Text style={styles.text}>However, despite the ongoing efforts to increase blood donations, there remains a constant need for more donors, especially in times of emergencies or natural disasters when the demand for blood rises
-         significantly.</Text>
-        <Text style={styles.text}>Various organizations, hospitals, and blood banks collaborate on World Blood Donor Day to organize events, blood drives, and campaigns to encourage more people to donate blood regularly. These initiatives aim to create a culture of voluntary blood donation and ensure a stable and sufficient blood supply to meet the healthcare needs of
-         communities worldwide.</Text>
-        <Text style={styles.text}>Ultimately, World Blood Donor Day serves as a reminder that every donation counts and has the power to make a difference in someone’s life. It celebrates the generosity of donors and highlights the ongoing need for their altruistic contributions to sustain a safe and adequate
-         blood supply for all.</Text>
+        <Text style={styles.text}>
+          BThe event was established by the World Health Organization (WHO) to
+          highlight the constant need for safe blood and blood products for
+          transfusion and to express gratitude to blood donors for their
+          life-saving gifts. The theme of World Blood Donor Day varies each
+          year, focusing on different aspects related to blood donation,
+          transfusion, and health.
+        </Text>
+        <Text style={styles.text}>
+          The significance of blood donation cannot be overstated. Donated blood
+          is vital for various medical procedures, including surgeries,
+          childbirth, trauma care, and treating chronic illnesses such as anemia
+          and cancer. It is also crucial for patients suffering from conditions
+          like thalassemia and hemophilia who require regular transfusions to
+          lead healthy lives.
+        </Text>
+        <Text style={styles.text}>
+          The theme for World Blood Donor Day often emphasizes raising awareness
+          and encouraging more people, especially young individuals, to become
+          regular blood donors. Educating communities about the importance of
+          safe blood donation practices, addressing misconceptions, and
+          promoting voluntary, unpaid donations are essential components of
+          these campaigns.
+        </Text>
+        <Text style={styles.text}>
+          Donating blood is a simple yet impactful act that has the potential to
+          save lives. It’s often said that a single donation can save up to
+          three lives, as blood can be separated into its components – red blood
+          cells, plasma, and platelets – and used for different patients in
+          need.
+        </Text>
+        <Text style={styles.text}>
+          However, despite the ongoing efforts to increase blood donations,
+          there remains a constant need for more donors, especially in times of
+          emergencies or natural disasters when the demand for blood rises
+          significantly.
+        </Text>
+        <Text style={styles.text}>
+          Various organizations, hospitals, and blood banks collaborate on World
+          Blood Donor Day to organize events, blood drives, and campaigns to
+          encourage more people to donate blood regularly. These initiatives aim
+          to create a culture of voluntary blood donation and ensure a stable
+          and sufficient blood supply to meet the healthcare needs of
+          communities worldwide.
+        </Text>
+        <Text style={styles.text}>
+          Ultimately, World Blood Donor Day serves as a reminder that every
+          donation counts and has the power to make a difference in someone’s
+          life. It celebrates the generosity of donors and highlights the
+          ongoing need for their altruistic contributions to sustain a safe and
+          adequate blood supply for all.
+        </Text>
       </View>
     </ScrollView>
   );
@@ -48,11 +92,11 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center', // Center content vertically if needed
+    backgroundColor: "white",
+    alignItems: "center", // Center content vertically if needed
   },
   image: {
-    width: '95%', // Adjust width as needed
+    width: "95%", // Adjust width as needed
     height: 250, // Adjust height as needed
     resizeMode: "cover", // or "contain" or "stretch"
     borderWidth: 0.8,
@@ -65,7 +109,7 @@ const styles = StyleSheet.create({
     color: "#333",
     marginTop: 15,
     margin: 10,
-    lineHeight: 24
+    lineHeight: 24,
   },
 });
 
